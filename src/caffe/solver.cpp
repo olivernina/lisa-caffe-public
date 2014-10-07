98ce14b8a5873214f239cfa0030657f69598b819
Jeff Donahue <jeff.donahue@gmail.com>
2014-10-06 23:46:48 -0700

    proto/solver: add gradient clipping options (TODO: get rid of excessive log messages)

--- src/caffe/solver.cpp
+++ src/caffe/solver.cpp
@@ -438,6 +438,36 @@ void SGDSolver<Dtype>::ComputeUpdateValue() {
   }
   Dtype momentum = this->param_.momentum();
   Dtype weight_decay = this->param_.weight_decay();
+  Dtype clip_gradients = this->param_.clip_gradients();
+  if (clip_gradients >= 0) {
+    Dtype sumsq_diff = 0;
+    for (int i = 0; i < net_params.size(); ++i) {
+      if (this->net_->param_owners()[i] < 0) {
+        sumsq_diff += net_params[i]->sumsq_diff();
+      }
+    }
+    const Dtype l2norm_diff = std::sqrt(sumsq_diff);
+    if (l2norm_diff > clip_gradients) {
+      Dtype scale_factor = clip_gradients / l2norm_diff;
+      LOG(ERROR) << "Gradient clipping: scaling down gradients (L2 norm "
+          << l2norm_diff << " > " << clip_gradients << ") "
+          << "by scale factor " << scale_factor;
+      for (int i = 0; i < net_params.size(); ++i) {
+        if (this->net_->param_owners()[i] < 0) {
+          net_params[i]->scale_diff(scale_factor);
+        }
+      }
+      sumsq_diff = 0;
+      for (int i = 0; i < net_params.size(); ++i) {
+        if (this->net_->param_owners()[i] < 0) {
+          sumsq_diff += net_params[i]->sumsq_diff();
+        }
+      }
+      const Dtype new_l2norm_diff = std::sqrt(sumsq_diff);
+      LOG(ERROR) << "Gradient clipping: scaled down gradients (new L2 norm "
+          << new_l2norm_diff << ")";
+    }
+  }
   string regularization_type = this->param_.regularization_type();
   switch (Caffe::mode()) {
   case Caffe::CPU:
