e43d729ed8527115c6e9b1bb6053b823eea41438
Jeff Donahue <jeff.donahue@gmail.com>
2014-11-06 16:10:54 -0800

    create_flickr30k_lmdb.sh improved

--- examples/lstm/flickr/create_flickr30k_lmdb.sh
+++ examples/lstm/flickr/create_flickr30k_lmdb.sh
@@ -1,25 +1,33 @@
 #!/usr/bin/env bash
+IM_ROOT=./
 ROOT=\
-./
+./cocoflickr/coco_flickr30k_hdf5/buffer_100_maxwords_20
 TRAIN_LISTFILE=\
-./cocoflickr/flickr30k_hdf5/buffer_100_maxwords_20/train_batches/image_list.with_dummy_labels.txt
+$ROOT/train_batches/image_list.with_dummy_labels.txt
+OUT_DB_ROOT=\
+$ROOT/lmdb
 TRAIN_OUT_DB_NAME=\
-./cocoflickr/flickr30k_lmdb/train
+$OUT_DB_ROOT/train
 VAL_LISTFILE=\
-./cocoflickr/flickr30k_hdf5/buffer_100_maxwords_20/valid_batches/image_list.with_dummy_labels.txt
+$ROOT/valid_batches/image_list.with_dummy_labels.txt
 VAL_OUT_DB_NAME=\
-./cocoflickr/flickr30k_lmdb/valid
+$OUT_DB_ROOT/valid
 
 RESIZE_HEIGHT=256
 RESIZE_WIDTH=256
 BACKEND=lmdb
 
-../../../build/tools/convert_imageset $ROOT $TRAIN_LISTFILE $TRAIN_OUT_DB_NAME \
-  -resize_height $RESIZE_HEIGHT \
-  -resize_width $RESIZE_WIDTH \
-  -backend $BACKEND
-
-../../../build/tools/convert_imageset $ROOT $VAL_LISTFILE $VAL_OUT_DB_NAME \
-  -resize_height $RESIZE_HEIGHT \
-  -resize_width $RESIZE_WIDTH \
-  -backend $BACKEND
+if [ -e "$OUT_DB_ROOT" ]; then
+  echo "Error: output dir exists: $OUT_DB_ROOT"
+  exit 1
+else
+  mkdir -p "$OUT_DB_ROOT"
+  ../../../build/tools/convert_imageset $IM_ROOT $TRAIN_LISTFILE $TRAIN_OUT_DB_NAME \
+    -resize_height $RESIZE_HEIGHT \
+    -resize_width $RESIZE_WIDTH \
+    -backend $BACKEND
+  ../../../build/tools/convert_imageset $IM_ROOT $VAL_LISTFILE $VAL_OUT_DB_NAME \
+    -resize_height $RESIZE_HEIGHT \
+    -resize_width $RESIZE_WIDTH \
+    -backend $BACKEND
+fi
