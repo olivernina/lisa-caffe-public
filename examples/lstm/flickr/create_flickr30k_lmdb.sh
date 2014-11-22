8e846e3cbdbbe2b35ab10309ff45630d353ad0b5
Jeff Donahue <jeff.donahue@gmail.com>
2014-11-21 17:33:11 -0800

    create_flickr30k_lmdb.sh: let's do this one at a time

--- examples/lstm/flickr/create_flickr30k_lmdb.sh
+++ examples/lstm/flickr/create_flickr30k_lmdb.sh
@@ -1,32 +1,26 @@
 #!/usr/bin/env bash
+SET_NAME=train_flickr
 IM_ROOT=./
 ROOT=\
 ./cocoflickr/coco_flickr30k_hdf5/buffer_100_maxwords_20
-TRAIN_LISTFILE=\
-$ROOT/train_batches/image_list.with_dummy_labels.txt
+LISTFILE=\
+$ROOT/${SET_NAME}_batches/image_list.with_dummy_labels.txt
 OUT_DB_ROOT=\
-$ROOT/lmdb
-TRAIN_OUT_DB_NAME=\
-$OUT_DB_ROOT/train
-VAL_LISTFILE=\
-$ROOT/valid_batches/image_list.with_dummy_labels.txt
-VAL_OUT_DB_NAME=\
-$OUT_DB_ROOT/valid
+$ROOT/lmdb/${SET_NAME}
+OUT_DB_NAME=\
+${OUT_DB_ROOT}/${SET_NAME}
 
 RESIZE_HEIGHT=256
 RESIZE_WIDTH=256
 BACKEND=lmdb
 
-if [ -e "$OUT_DB_ROOT" ]; then
-  echo "Error: output dir exists: $OUT_DB_ROOT"
+if [ -e "$OUT_DB_NAME" ]; then
+  echo "Error: output dir exists: $OUT_DB_NAME"
   exit 1
 else
   mkdir -p "$OUT_DB_ROOT"
-  ../../../build/tools/convert_imageset $IM_ROOT $TRAIN_LISTFILE $TRAIN_OUT_DB_NAME \
-    -resize_height $RESIZE_HEIGHT \
-    -resize_width $RESIZE_WIDTH \
-    -backend $BACKEND
-  ../../../build/tools/convert_imageset $IM_ROOT $VAL_LISTFILE $VAL_OUT_DB_NAME \
+  ../../../build/tools/convert_imageset \
+    $IM_ROOT $LISTFILE $OUT_DB_NAME \
     -resize_height $RESIZE_HEIGHT \
     -resize_width $RESIZE_WIDTH \
     -backend $BACKEND
