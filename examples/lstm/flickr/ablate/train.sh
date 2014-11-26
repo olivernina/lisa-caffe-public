4d927de99e1e7fad0c30fd3474f666e2ffc9972f
Jeff Donahue <jeff.donahue@gmail.com>
2014-11-25 16:35:12 -0800

    ablate/train.sh can resume from snapshot, add dropafterdup prototxts

--- examples/lstm/flickr/ablate/train.sh
+++ examples/lstm/flickr/ablate/train.sh
@@ -2,23 +2,30 @@
 
 SOLVER=$1
 GPU=$2
+SNAPSHOT=$3
 
 if [ -z "$GPU" ]; then
   echo "Usage:"
-  echo "\ttrain.sh <solver> <GPU ID>"
+  echo "train.sh <solver> <GPU ID>"
   exit 1
 fi
 
 WEIGHTS=\
 ./bvlc_reference_caffenet.caffemodel
 
-LOG=\
-./logs/ablate/$(basename ${SOLVER}).train_log.txt
+LOG=./logs/ablate/$(basename ${SOLVER})
+if [ -z "$SNAPSHOT" ]; then
+  INIT_ARG="--weights=${WEIGHTS}"
+else
+  LOG=${LOG}.continue.$(basename ${SNAPSHOT})
+  INIT_ARG="--snapshot=${SNAPSHOT}"
+fi
+LOG=${LOG}.train_log.txt
 
 echo "Training, logging to: ${LOG}"
 
 caffe train \
- -weights ${WEIGHTS} \
+ ${INIT_ARG} \
  -solver ${SOLVER} \
  -gpu ${GPU} \
  > ${LOG} 2>&1
