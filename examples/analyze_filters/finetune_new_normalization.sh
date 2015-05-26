15a88135fb4ece57c6c81ad16abe57f32a219505
LisaAnne <lisa.a.hendricks@gmail.com>
2015-05-25 23:41:17 -0700

    Added imagenet examples

--- examples/analyze_filters/finetune_new_normalization.sh
+++ examples/analyze_filters/finetune_new_normalization.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 ../../build/tools/caffe train \
-    --solver=imagenet_new_normalization.prototxt --model=/x/data/Caffenets/alexnet_seed1/caffe_alexnet_train_iter_300000.caffemodel > imagenet_new_normalization.out 2>&1 &
+    --solver=imagenet_new_normalization.prototxt --model=/x/data/Caffenets/alexnet_seed1/caffe_alexnet_train_iter_300000.caffemodel > imagenet_new_normalization_lr0p1_msra.out 2>&1 &
